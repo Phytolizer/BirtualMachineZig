63f8f62736db7d5c277f3f4f7f2b81308788aeed
Kyle Coffey <me@phytolizer.dev>
2022-08-22 02:19:32 -0500

    add debugging to bme

--- src/bme.zig
+++ src/bme.zig
@@ -5,7 +5,13 @@ const args = @import("args");
 const Machine = libbm.Machine;
 
 fn usage(comptime Writer: type, writer: Writer, executableName: []const u8) !void {
-    try writer.print("Usage: {s} <-i input.bm> [-l limit]\n", .{executableName});
+    try writer.print("Usage: {s} <-i input.bm> [-l limit] [-d]\n", .{executableName});
+}
+
+fn ignoreLine(reader: anytype) !bool {
+    var buf: [1024]u8 = undefined;
+    var line = try reader.readUntilDelimiterOrEof(&buf, '\n');
+    return line != null;
 }
 
 pub fn main() !void {
@@ -19,11 +25,13 @@ pub fn main() !void {
         input: ?[]const u8 = null,
         limit: ?usize = null,
         help: bool = false,
+        debug: bool = false,
 
         pub const shorthands = .{
             .i = "input",
             .l = "limit",
             .h = "help",
+            .d = "debug",
         };
     }, allocator, .silent) catch |e| {
         try usage(@TypeOf(stderr), stderr, "bme");
@@ -42,10 +50,25 @@ pub fn main() !void {
 
     const inputFilePath = parsed.options.input.?;
 
-    var bm = try Machine.initFromFile(inputFilePath);
-    bm.executeProgram(parsed.options.limit) catch |e| {
-        try bm.dumpStack(@TypeOf(stderr), stderr);
-        return e;
-    };
-    try bm.dumpStack(@TypeOf(stdout), stdout);
+    if (parsed.options.debug) {
+        var bm = try Machine.initFromFile(inputFilePath);
+        const limit = parsed.options.limit;
+        var i: usize = 0;
+        var buf = std.ArrayList(u8).init(allocator);
+        defer buf.deinit();
+        while ((limit == null or i < limit.?) and !bm.halt) : (i += 1) {
+            try bm.dumpStack(@TypeOf(stdout), stdout);
+            if (!try ignoreLine(std.io.getStdIn().reader())) {
+                break;
+            }
+            try bm.executeInstruction();
+        }
+    } else {
+        var bm = try Machine.initFromFile(inputFilePath);
+        bm.executeProgram(parsed.options.limit) catch |e| {
+            try bm.dumpStack(@TypeOf(stderr), stderr);
+            return e;
+        };
+        try bm.dumpStack(@TypeOf(stdout), stdout);
+    }
 }
